221f10082df3053dbd3d6d165566cd9e06eb44d6
Max Risovany <BagsBoom>
2021-12-09 16:02:41 +0200

    Added Insert, Delete and Update to first table

--- DepartmentApp/DepartmentApp/departmentsForm.Designer.cs
+++ DepartmentApp/DepartmentApp/departmentsForm.Designer.cs
@@ -29,52 +29,83 @@ namespace DepartmentApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(departmentsForm));
             this.dataGridViewDepartments = new System.Windows.Forms.DataGridView();
-            this.dept_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dept_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartments)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridViewDepartments
             // 
             this.dataGridViewDepartments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewDepartments.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dept_no,
-            this.dept_name});
-            this.dataGridViewDepartments.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewDepartments.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewDepartments.Location = new System.Drawing.Point(0, 0);
             this.dataGridViewDepartments.Name = "dataGridViewDepartments";
-            this.dataGridViewDepartments.Size = new System.Drawing.Size(519, 332);
+            this.dataGridViewDepartments.Size = new System.Drawing.Size(653, 479);
             this.dataGridViewDepartments.TabIndex = 0;
+            this.dataGridViewDepartments.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDepartments_CellContentClick);
+            this.dataGridViewDepartments.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDepartments_CellValueChanged);
+            this.dataGridViewDepartments.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridViewDepartments_UserAddedRow);
             // 
-            // dept_no
+            // toolStrip1
             // 
-            this.dept_no.HeaderText = "dept_no";
-            this.dept_no.Name = "dept_no";
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(653, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // dept_name
+            // toolStripButton1
             // 
-            this.dept_name.HeaderText = "dept_name";
-            this.dept_name.Name = "dept_name";
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(49, 22);
+            this.toolStripButton1.Text = "Update";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(84, 22);
+            this.toolStripButton2.Text = "Back to Menu";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // departmentsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CadetBlue;
-            this.ClientSize = new System.Drawing.Size(543, 417);
+            this.ClientSize = new System.Drawing.Size(653, 479);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.dataGridViewDepartments);
             this.Name = "departmentsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Departments Table";
+            this.Load += new System.EventHandler(this.departmentsForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDepartments)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewDepartments;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dept_no;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dept_name;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
     }
 }
--- DepartmentApp/DepartmentApp/departmentsForm.cs
+++ DepartmentApp/DepartmentApp/departmentsForm.cs
@@ -13,34 +13,203 @@ namespace DepartmentApp
 {
     public partial class departmentsForm : Form
     {
+
+        private MySqlCommandBuilder mySqlCommandBuilder = null;
+        private MySqlDataAdapter mySqlDataAdapter = null;
+        private DataSet dataSet = null;
+        private bool newRowAdd = false;
         public departmentsForm()
         {
             InitializeComponent();
-            LoadData();
         }
         
         private void LoadData()
-        {   
-            MySqlConnection con = new MySqlConnection(DataBase.connectionSql());
-            con.Open();
-
-            string query = "SELECT * FROM departments";
-            MySqlCommand command = new MySqlCommand(query, con);
-            MySqlDataReader reader = command.ExecuteReader();
-
-            List<string[]> data = new List<string[]>();
-
-            while(reader.Read())
+        {
+            try
             {
-                data.Add(new string[2]);
-                data[data.Count - 1][0] = reader[0].ToString();
-                data[data.Count - 1][1] = reader[1].ToString();
-            }
-            reader.Close();
-            con.Close();
+                string query = "SELECT *, 'Delete' AS 'Delete' FROM departments";
+                MySqlConnection con = new MySqlConnection(DataBase.connectionSql());
 
-            foreach (string[] s in data)
-                dataGridViewDepartments.Rows.Add(s);
+                mySqlDataAdapter = new MySqlDataAdapter(query, con);
+                mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+
+                mySqlCommandBuilder.GetInsertCommand();
+                mySqlCommandBuilder.GetUpdateCommand();
+                mySqlCommandBuilder.GetDeleteCommand();
+
+                dataSet = new DataSet();
+
+                mySqlDataAdapter.Fill(dataSet, "departments");
+
+                dataGridViewDepartments.DataSource = dataSet.Tables["departments"];
+
+                for (int i = 0; i < dataGridViewDepartments.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridViewDepartments[2, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void ReloadData()
+        {
+            try
+            {
+                dataSet.Tables["departments"].Clear();
+
+                mySqlDataAdapter.Fill(dataSet, "departments");
+
+                dataGridViewDepartments.DataSource = dataSet.Tables["departments"];
+
+                for (int i = 0; i < dataGridViewDepartments.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridViewDepartments[2, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var Main = new Main_Form();
+            Main.Closed += (s, args) => this.Close();
+            Main.Show();
+        }
+
+        private void departmentsForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            ReloadData();
+        }
+
+        private void dataGridViewDepartments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 2)
+                {
+                    string task = dataGridViewDepartments.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+
+                    if(task == "Delete")
+                    {
+                        if(MessageBox.Show("Delete this record?", "Deleting", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
+                            == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+
+                            dataGridViewDepartments.Rows.RemoveAt(rowIndex);
+                            dataSet.Tables["departments"].Rows[rowIndex].Delete();
+
+                            mySqlDataAdapter.Update(dataSet, "departments");
+                        }
+                    }
+                    else if(task == "Insert")
+                    {
+                        int rowIndex = dataGridViewDepartments.Rows.Count - 2;
+
+                        DataRow row = dataSet.Tables["departments"].NewRow();
+
+                        row["dept_no"] = dataGridViewDepartments.Rows[rowIndex].Cells["dept_no"].Value;
+                        row["dept_name"] = dataGridViewDepartments.Rows[rowIndex].Cells["dept_name"].Value;
+
+                        dataSet.Tables["departments"].Rows.Add(row);
+                        dataSet.Tables["departments"].Rows.RemoveAt(dataSet.Tables["departments"].Rows.Count - 1);
+
+                        dataGridViewDepartments.Rows.RemoveAt(dataGridViewDepartments.Rows.Count - 2);
+                        dataGridViewDepartments.Rows[e.RowIndex].Cells[2].Value = "Delete";
+
+                        mySqlDataAdapter.Update(dataSet, "departments");
+
+                        newRowAdd = false;
+                    }
+                    else if(task == "Update")
+                    {
+                        int x = e.RowIndex;
+
+                        dataSet.Tables["departments"].Rows[x]["dept_no"] = dataGridViewDepartments.Rows[x].Cells["dept_no"].Value;
+                        dataSet.Tables["departments"].Rows[x]["dept_name"] = dataGridViewDepartments.Rows[x].Cells["dept_name"].Value;
+
+                        mySqlDataAdapter.Update(dataSet, "departments");
+                        dataGridViewDepartments.Rows[e.RowIndex].Cells[2].Value = "Delete";
+                    }
+
+                    ReloadData();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridViewDepartments_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+                if(newRowAdd == false)
+                {
+                    newRowAdd = true;
+
+                    int lastRow = dataGridViewDepartments.Rows.Count - 2;
+
+                    DataGridViewRow row = dataGridViewDepartments.Rows[lastRow];
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridViewDepartments[2, lastRow] = linkCell;
+
+                    row.Cells["Delete"].Value = "Insert";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridViewDepartments_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if(newRowAdd == false)
+                {
+                    int rowIndex = dataGridViewDepartments.SelectedCells[0].RowIndex;
+                    DataGridViewRow editingRow = dataGridViewDepartments.Rows[rowIndex];
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridViewDepartments[2, rowIndex] = linkCell;
+
+                    editingRow.Cells["Delete"].Value = "Update";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
